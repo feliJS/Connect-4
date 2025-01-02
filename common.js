7137315358702376a5c8bdf5c04d9c8a99bace5f
Felicia <190894036+feliJS@users.noreply.github.com>
2025-01-02 20:28:22 +0100

    select icon function
    added a function that can select an icon and then save it to an object in a list. I have a selectedPlayer that will change depending on the player we are on. It starts at 0 and will later get changed when its the next players turn. The iconSelector() will loop thru all the icon buttons and add an eventlistener to each one, if you click on the button it will firstly reset all the borders that are currently there, and then add a border to the one you have clicked. i wanted a border cause its easier to see which one you clicked on at the moment. Then i will set the player 1 icon to the icon you clicked on

--- common.js
+++ common.js
@@ -1,72 +1,19 @@
-function randomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
-function createBox() {
-    const numBoxDOM = document.createElement("div")
-    numBoxDOM.classList.add("box")
-    numBoxDOM.style.display = "flex"
-    numBoxDOM.style.justifyContent = "center"
-    numBoxDOM.style.alignItems = "center"
-    numBoxDOM.textContent = randomNum(0, 100)
-    handleBox(numBoxDOM)
-    numBoxesDOM.appendChild(numBoxDOM)
-}
+const players = [{}, {}];
+let selectedPlayer = 0;
 
-function autoFill(amount) {
-    for (let i = 0; i < amount; i++) {
-        createBox();
-    }
-    handleCreation();
-}
 
-function markNum(num) {
-    const boxes = document.querySelectorAll(".box")
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].classList.remove("marked")
-        if (boxes[i].textContent == num) {
-            boxes[i].classList.add("marked")
-        }
-    }
-}
-
-function removeClassFromAll(otherBoxModifyFunc) {
-    const boxes = document.querySelectorAll(".box")
-    for (let i = 0; i < boxes.length; i++) {
-        if (boxes[i].classList.contains("marked")) {
-            boxes[i].classList.remove("marked")
-            if (otherBoxModifyFunc) {
-                otherBoxModifyFunc(boxes[i])
+function iconSelector() {
+    iconBtns = document.querySelectorAll("#player-icon-section button");
+    for (let i = 0; i < iconBtns.length; i++) {
+        iconBtns[i].addEventListener("click", () => {
+            for (let x = 0; x < iconBtns.length; x++) {
+                iconBtns[x].style.border = "none";
             }
-        }
+            iconBtns[i].style.border = "2px solid tomato";
+            players[selectedPlayer].icon = iconBtns[i]
+        })
     }
 }
 
-const homeLinkDOM = document.createElement("a")
-homeLinkDOM.setAttribute("href", "../index.html")
-homeLinkDOM.textContent = "Home"
-document.getElementsByClassName("center")[0].appendChild(homeLinkDOM)
-
-const createTextInfoDOM = document.createElement("p")
-createTextInfoDOM.textContent = "How many numbers in the grid?"
-document.getElementById("create-nums").appendChild(createTextInfoDOM)
-
-
-const inputDOM = document.createElement("input")
-inputDOM.setAttribute("type", "text")
-document.getElementById("create-nums").appendChild(inputDOM)
-inputDOM.value = "95"
-
-const createButtonDOM = document.createElement("button")
-createButtonDOM.textContent = "Create"
-document.getElementById("create-nums").appendChild(createButtonDOM)
-
-const numBoxesDOM = document.querySelector("#num-boxes")
-
-autoFill(95);
-
-document.querySelector("#create-nums button").addEventListener("click", () => {
-    numBoxesDOM.innerHTML = "";
-    autoFill(parseInt(inputDOM.value))
-
-})
+iconSelector();
