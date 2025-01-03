82ee26ebb73a8926bc0e6ca7a7dfcea2fa63b797
Felicia <190894036+feliJS@users.noreply.github.com>
2025-01-03 16:57:41 +0100

    startGame() that displays icons and player names
    Firstly i added the scoreBoard DOM element in the players, i did this so its easier to append the username and images later, and also in the future when im doing the scoring i can easier access each DOM element and just add a <p> for the score etc. I used this in the startGame() function that has a for loop for each player in players, doing this i can easier append text and the image on both sides instead of repeating it and having unnessecary long code. now i can take the icon from the objects which i saved before, add it to the scoreBoard on each side with appendChild, then i will create an h2 and add textContent which will be each players userName and finally append it to the scoreBoard. I will run this function directly after the choosePlayerInfoSectionDOM is none, because then the game will actually "start"

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 
-const players = [{}, {}];
+const players = [{ scoreBoard: document.querySelector("#player1-score-board") }, { scoreBoard: document.querySelector("#player2-score-board") }];
 let selectedPlayer = 0;
 
 
@@ -16,19 +16,30 @@ function iconSelector() {
     }
 }
 
-function setPlayerName() {
-    const inputDOM = document.querySelector("#player-selection input");
-    const playerName = inputDOM.value;
-    players[selectedPlayer].username = playerName;
+function startGame() {
+    for (let i = 0; i < players.length; i++) {
+        playerIcon = players[i].icon
+        players[i].scoreBoard.appendChild(playerIcon);
+        const playerNameDOM = document.createElement("h2")
+        playerNameDOM.textContent = players[i].username;
+        players[i].scoreBoard.appendChild(playerNameDOM);
+    }
 }
 
 iconSelector();
+
 const playerContinueBtn = document.getElementById("continue");
+const inputDOM = document.querySelector("#player-selection input");
+
 playerContinueBtn.addEventListener("click", () => {
-    setPlayerName();
+    let playerName = inputDOM.value;
+    players[selectedPlayer].username = playerName;
+    inputDOM.value = "";
     selectedPlayer++;
     if (selectedPlayer === 2) {
         const choosePlayerInfoSectionDOM = document.querySelector("#player-selection");
         choosePlayerInfoSectionDOM.style.display = "none";
+        startGame();
     }
 })
+
